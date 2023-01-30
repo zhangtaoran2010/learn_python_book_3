ba81c5f3fc65e3b1f7cebf1d3e2646114806b7c2
zhangtaoran2010 <123609943+zhangtaoran2010@users.noreply.github.com>
2023-01-30 08:15:23 +0800

    Add files via upload

---
+++ 强化训练 第3章/3.23.py
@@ -0,0 +1,4 @@
+print("                        Google")
+print("")
+addr = input("在Google上搜索，或者输入一个网址")
+print("https://www.ea.com/fifa/ultimate-team/web-app/")
---
+++ 强化训练 第3章/3.26.py
@@ -0,0 +1,4 @@
+str = "Alt + 41420"
+key = input("请输入快捷键：")
+if key == str:
+    print("√")
